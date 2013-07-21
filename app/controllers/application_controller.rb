43b6f4e7eca9c6a532e1cb65d5a0265549991c19
hebinbin <hebinbin0905@gmail.com>
2013-07-21 19:18:17 +0900

    add imageuploader function

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  # image uploader
+  def image
+    gridfs_path = env["PATH_INFO"].gsub("/images/", "")
+    begin
+      gridfs_file = Mongo::GridFileSystem.new(Mongoid.database).open(gridfs_path, 'r')
+      self.response_body = gridfs_file.read
+      self.content_type = gridfs_file.content_type
+    rescue
+      self.status = :file_not_found
+      self.content_type = 'text/plain'
+      self.response_body = ''
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeaLife::Application.config.secret_token = 'c96860ca22a57a59c181fb3ce3ca59c693f06d1f054a024f9a7b6491127d7039e7730ad7d59a4bb12cde23f353f8668406f23f9d083e80f437389d602e2c438c'
